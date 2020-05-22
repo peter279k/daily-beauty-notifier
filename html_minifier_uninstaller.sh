81b1a80ad8775880d4bf998614e5d659efb2f63b
peter279k <peter279k@gmail.com>
2020-05-22 22:35:35 +0800

    Remove html_minifier_uninstaller.sh script

--- html_minifier_uninstaller.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'In only supports for Bash script!'
-
-rm -rf ${HOME}/.nvm
-
-sed -i -e 's@export NVM_DIR="$HOME/.nvm"@@g' "${HOME}/.bashrc"
-sed -i -e 's@[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm@@g' "${HOME}/.bashrc"
-sed -i -e 's@[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion@@g' "${HOME}/.bashrc"
