a6a90560f5212c13624ac78deab681761f2eb9fb
peter279k <peter279k@gmail.com>
2020-05-22 22:39:10 +0800

    Improve loading "${HOME}/.nvm/nvm.sh" NVM

--- html_minifier_installer.sh
+++ html_minifier_installer.sh
@@ -4,21 +4,21 @@ echo "Download nvm script....."
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-source "${HOME}/.bashrc" && nvm install stable
-source "${HOME}/.bashrc" && nvm use stable
+source "${HOME}/.nvm/nvm.sh" && nvm install stable
+source "${HOME}/.nvm/nvm.sh" && nvm use stable
 
 echo "Check Node.js version....."
 
-source "${HOME}/.bashrc" && node --version
+source "${HOME}/.nvm/nvm.sh" && node --version
 
 echo "Check NPM version....."
 
-source "${HOME}/.bashrc" && npm --version
+source "${HOME}/.nvm/nvm.sh" && npm --version
 
 echo "Install html-minifier package globally....."
 
-source "${HOME}/.bashrc" && npm install html-minifier -g
+source "${HOME}/.nvm/nvm.sh" && npm install html-minifier -g
 
 echo "Check uglify-js command....."
 
-source "${HOME}/.bashrc" && uglifyjs --version
+source "${HOME}/.nvm/nvm.sh" && uglifyjs --version
