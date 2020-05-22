4b6da876f7fde858556835a5ec3866b669c517e6
peter279k <peter279k@gmail.com>
2020-05-22 22:30:26 +0800

    Improve source .bashrc and executable uninstaller

--- html_minifier_installer.sh
+++ html_minifier_installer.sh
@@ -4,21 +4,21 @@ echo "Download nvm script....."
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-source ${HOME}/.bashrc && nvm install stable
-source ${HOME}/.bashrc && nvm use stable
+source "${HOME}/.bashrc" && nvm install stable
+source "${HOME}/.bashrc" && nvm use stable
 
 echo "Check Node.js version....."
 
-source ${HOME}/.bashrc && node --version
+source "${HOME}/.bashrc" && node --version
 
 echo "Check NPM version....."
 
-source ${HOME}/.bashrc && npm --version
+source "${HOME}/.bashrc" && npm --version
 
 echo "Install html-minifier package globally....."
 
-source ${HOME}/.bashrc && npm install html-minifier -g
+source "${HOME}/.bashrc" && npm install html-minifier -g
 
 echo "Check uglify-js command....."
 
-source ${HOME}/.bashrc && uglifyjs --version
+source "${HOME}/.bashrc" && uglifyjs --version
